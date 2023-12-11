1c546c7a17210277ab8b3e379f90e296eb9654d6
Alfonso Cabezas Fernández <alfonso.acf66@gmail.com>
2023-12-11 19:28:15 +0100

    Updates

--- App.tsx
+++ App.tsx
@@ -1,25 +1,90 @@
-// App.tsx
-import React from 'react';
-import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import Navigator from './src/navigation/Navigator';
+// src/screens/HomeScreen.tsx
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Button, Platform, PermissionsAndroid, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const HomeScreen = ({ navigation }) => {
+  const [currentRegion, setCurrentRegion] = useState(null);
 
-  const backgroundStyle = {
-    flex: 1,
-    backgroundColor: isDarkMode ? '#333' : '#FFF',
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      getOneTimeLocation();
+    } else {
+      requestLocationPermission();
+    }
+  }, []);
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Permission',
+          message: 'This app needs access to your location',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        getOneTimeLocation();
+      } else {
+        Alert.alert('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const getOneTimeLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setCurrentRegion({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        });
+      },
+      (error) => {
+        console.warn(error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NavigationContainer>
-        <Navigator />
-      </NavigationContainer>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button title="Crear Ruta" onPress={() => console.log('Crear Ruta')} />
+        <Button title="Cargar Ruta" onPress={() => console.log('Cargar Ruta')} />
+      </View>
+      {currentRegion && (
+        <MapView
+          style={styles.map}
+          region={currentRegion}
+          showsUserLocation
+        >
+          <Marker coordinate={currentRegion} />
+        </MapView>
+      )}
+    </View>
   );
 };
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default HomeScreen;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,89 @@
-// src/screens/HomeScreen.tsx
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../types/navigationTypes';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Button, Platform, PermissionsAndroid, Alert } from 'react-native';
+import MapView, { Marker, Region } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
-type HomeScreenProps = {
-  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
-};
+const HomeScreen = ({ navigation }) => {
+  const [currentRegion, setCurrentRegion] = useState<Region | null>(null); // Especifica el tipo de Region
 
-const HomeScreen = ({ navigation }: HomeScreenProps) => {
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Location Permission',
+            message: 'This app needs access to your location.',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          getOneTimeLocation();
+        } else {
+          Alert.alert('Location permission denied');
+        }
+      } catch (err) {
+        Alert.alert('Permission error', err.toString()); // Convierte err a string
+      }
+    } else {
+      getOneTimeLocation();
+    }
+  };
+
+  const getOneTimeLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setCurrentRegion({
+            latitude: currentRegion?.latitude || 0,
+            longitude: currentRegion?.longitude || 0,
+            latitudeDelta: 1, // Aumenta este valor
+            longitudeDelta: 1, // Aumenta este valor
+        });
+      },
+      (error) => {
+        Alert.alert('Location error', error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    );
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Map"
-        onPress={() => navigation.navigate('Map')}
-      />
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button title="Crear Ruta" onPress={() => console.log('Crear Ruta')} />
+        <Button title="Cargar Ruta" onPress={() => console.log('Cargar Ruta')} />
+      </View>
+      {currentRegion && (
+        <MapView
+          style={styles.map}
+          initialRegion={currentRegion} // Usa initialRegion en lugar de region
+          showsUserLocation
+        >
+          <Marker coordinate={currentRegion} />
+        </MapView>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10,
+  },
+  map: {
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
