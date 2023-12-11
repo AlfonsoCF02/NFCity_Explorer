fd531d2e4a443c902cf34e8757b9ff3d7a473284
Alfonso Cabezas Fernández <alfonso.acf66@gmail.com>
2023-12-11 18:05:47 +0100

    Pruebas
    Git de prueba para inicio

---
+++ src/screens/MapScreen.tsx
@@ -0,0 +1,14 @@
+// src/screens/MapScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MapScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Map Screen</Text>
+      {/* Aquí agregarás la lógica del mapa */}
+    </View>
+  );
+};
+
+export default MapScreen;
---
+++ src/types/navigationTypes.ts
@@ -0,0 +1,8 @@
+// src/types/navigationTypes.ts
+export type RootStackParamList = {
+    Home: undefined;
+    Map: undefined;  // Añade parámetros aquí si tu pantalla Map los necesita
+  };
+  
+  // Añade más rutas y sus parámetros aquí si es necesario
+  
