5d09f6cddef222db6009068a0f873e2d7103d85a
Alfonso Cabezas Fernández <alfonso.acf66@gmail.com>
2024-01-02 11:31:56 +0100

    Update OptimiceRouteScreen.tsx

--- src/screens/OptimiceRouteScreen.tsx
+++ src/screens/OptimiceRouteScreen.tsx
@@ -1,5 +1,17 @@
 // CreateRouteScreen.tsx
 
+import React from 'react';
+import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+
+
+const OptimiceRouteScreen: React.FC = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView style={{ flex: 1 }} />
+    </View>
+  );
+};
 
 //Importa un mapa del telefono o drive y optimiza la ruta de los puntos de ese mapa
 
