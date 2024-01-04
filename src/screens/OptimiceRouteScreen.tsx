f526f649f4cd9f4e6bc0b05085a59797a170133c
Abraham Cordoba Perez <82827135+abrahamcp02@users.noreply.github.com>
2024-01-04 10:19:34 +0100

    SeleccionarArchivo

--- src/screens/OptimiceRouteScreen.tsx
+++ src/screens/OptimiceRouteScreen.tsx
@@ -1,19 +1,34 @@
-// CreateRouteScreen.tsx
-
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Button, Alert } from 'react-native';
 import MapView from 'react-native-maps';
-
+import DocumentPicker from 'react-native-document-picker';
 
 const OptimiceRouteScreen: React.FC = () => {
+
+  const selectFile = async () => {
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
+      });
+
+      console.log('Archivo seleccionado:', res);
+      // Aquí puedes agregar lógica para leer y procesar el archivo
+
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        console.log('Selección cancelada');
+      } else {
+        Alert.alert('Error', 'Ocurrió un error al seleccionar el archivo.');
+      }
+    }
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <MapView style={{ flex: 1 }} />
+      <Button title="Seleccionar archivo" onPress={selectFile} />
     </View>
   );
 };
 
-//Importa un mapa del telefono o drive y optimiza la ruta de los puntos de ese mapa
-
-//Importante exportarlo bien
 export default OptimiceRouteScreen;
