cd57a78406110da488561426ea5ed0ff49249d30
Alfonso Cabezas Fernández <alfonso.acf66@gmail.com>
2024-01-05 16:49:23 +0100

    Optimice Todo Menos Optimizador

--- src/screens/OptimiceRouteScreen.tsx
+++ src/screens/OptimiceRouteScreen.tsx
@@ -1,83 +1,145 @@
-import React, { useState } from 'react';
-import { View, Button, StyleSheet, Alert, Platform } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, StyleSheet, Button, Alert, Modal, Text, TextInput, TouchableOpacity, Platform } from 'react-native';
+import MapView, { Marker, LatLng } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 import DocumentPicker, { types } from 'react-native-document-picker';
 import { parseString } from 'xml2js';
 import RNFS from 'react-native-fs';
-import { PermissionsAndroid } from 'react-native';
 
-const OptimiceRouteScreen = () => {
-  const [fileUri, setFileUri] = useState<string>('');
+interface IMarker {
+  title: string;
+  coordinates: LatLng;
+}
 
-  const selectKMLFile = async () => {
+const OptimizeRouteScreen: React.FC = () => {
+  const [markers, setMarkers] = useState<IMarker[]>([]);
+  const [mapName, setMapName] = useState<string>('');
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [currentRegion, setCurrentRegion] = useState<MapView.Region | undefined>(undefined);
+  const mapRef = useRef<MapView | null>(null);
+
+  useEffect(() => {
+    getOneTimeLocation();
+  }, []);
+
+  const getOneTimeLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        setCurrentRegion({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        });
+      },
+      error => Alert.alert('Error', 'No se pudo obtener la ubicación'),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  };
+
+  const selectAndParseKMLFile = async () => {
     try {
-      // DocumentPickerResponse[] is an array
-      const results = await DocumentPicker.pick({
-        type: [types.allFiles],
-      });
-
-      // Get the first file from the array
+      const results = await DocumentPicker.pick({ type: [types.allFiles] });
       const res = results[0];
-
-      console.log('File picked:', res);
-
-      if (res && res.uri) {
-        if (Platform.OS === 'android') {
-          const newFilePath = `${RNFS.DocumentDirectoryPath}/${res.name}`;
-          await RNFS.copyFile(res.uri, newFilePath);
-          setFileUri(newFilePath);
-        } else {
-          setFileUri(res.uri);
-        }
+      setMapName(res.name?.split('.')[0] || '');
+      if (res.uri) {
+        const uri = Platform.OS === 'android' ? `file://${res.uri}` : res.uri;
+        const kmlData = await RNFS.readFile(uri, 'utf8');
+        parseString(kmlData, (err: any, result) => {
+          if (err) {
+            console.error('Error parsing KML:', err);
+            return;
+          }
+          const placemarks = result.kml.Document[0].Placemark;
+          const newMarkers: IMarker[] = placemarks.map(placemark => {
+            const name = placemark.name[0];
+            const coordinates = placemark.Point[0].coordinates[0].split(',');
+            return {
+              title: name,
+              coordinates: {
+                latitude: parseFloat(coordinates[1]),
+                longitude: parseFloat(coordinates[0]),
+              },
+            };
+          });
+          setMarkers(newMarkers);
+          if (newMarkers.length > 0 && mapRef.current) {
+            const coordinates = newMarkers.map(marker => marker.coordinates);
+            mapRef.current.fitToCoordinates(coordinates, {
+              edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+              animated: true,
+            });
+          }
+        });
       } else {
-        Alert.alert('Error', 'URI not found in the response');
+        Alert.alert('Error', 'URI no encontrada en la respuesta');
       }
     } catch (err) {
-      if (DocumentPicker.isCancel(err)) {
-        console.log('User cancelled the file picker');
-      } else {
-        console.error('Error picking file:', err);
-        Alert.alert('Error picking file:', err.message);
+      if (!DocumentPicker.isCancel(err)) {
+        console.error('Error al seleccionar el archivo:', err);
+        Alert.alert('Error al seleccionar el archivo:', err.message);
       }
     }
   };
-
-  const parseKML = async () => {
-    if (!fileUri) {
-      Alert.alert('Error', 'No file selected');
-      return;
-    }
-
-    try {
-      const kmlData = await RNFS.readFile(fileUri, 'utf8');
-      parseString(kmlData, (err, result) => {
-        if (err) {
-          console.error('Error parsing KML:', err);
-          return;
-        }
-
-        const placemarks = result.kml.Document[0].Placemark;
-        const parsedData = placemarks.map(placemark => {
-          const name = placemark.name[0];
-          const coordinates = placemark.Point[0].coordinates[0].split(',');
-          return {
-            name,
-            latitude: parseFloat(coordinates[1]),
-            longitude: parseFloat(coordinates[0])
-          };
-        });
-
-        console.log('Parsed data:', parsedData);
-      });
-    } catch (err) {
-      console.error('Error reading file:', err);
-      Alert.alert('Error reading file:', err.message);
-    }
+  const optimizeRoute = () => {
+    console.log('Optimizar Ruta');
   };
 
   return (
     <View style={styles.container}>
-      <Button title="Select KML File" onPress={selectKMLFile} />
-      <Button title="Parse KML" onPress={parseKML} />
+      <TextInput
+        placeholder="Nombre de la ruta"
+        value={mapName}
+        editable={false}
+        style={styles.mapNameInput}
+      />
+
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        showsUserLocation={true}
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.coordinates}
+            title={marker.title}
+          />
+        ))}
+      </MapView>
+
+      <View style={styles.buttonContainer}>
+        <Button title="Cargar Ruta" onPress={selectAndParseKMLFile} />
+
+      <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.aboutUsButton}>
+        <Text style={styles.aboutUsText}>Info</Text>
+      </TouchableOpacity>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.centeredModalView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>
+              Aquí puedes seleccionar un archivo KML para cargar los marcadores en el mapa.
+              Luego, puedes utilizar la funcionalidad de optimización de ruta.
+            </Text>
+            <TouchableOpacity
+              style={styles.closeModalButton}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.closeModalButtonText}>Cerrar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <Button title="Optimizar Ruta" onPress={optimizeRoute} />
+      </View>
+
     </View>
   );
 };
@@ -85,9 +147,67 @@ const OptimiceRouteScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#ADD8E6',
+  },
+  map: {
+    flex: 1,
+  },
+  mapNameInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    color: 'black',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+  },
+  aboutUsButton: {
+    marginTop: 10,
+    marginBottom: 10,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  aboutUsText: {
+    color: '#0000ff',
+    textDecorationLine: 'underline',
+  },
+  centeredModalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  closeModalButton: {
+    backgroundColor: '#2196F3',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  closeModalButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
 
-export default OptimiceRouteScreen;
+export default OptimizeRouteScreen;
