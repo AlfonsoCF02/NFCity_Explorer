3eb68cbbd2d635652da3098f018ab256b06410f3
Alfonso Cabezas Fernández <alfonso.acf66@gmail.com>
2024-01-06 18:16:03 +0100

    Optimizador Casi Completo
    Añadido el selector de inicio y fin con desplegable y el boton de instrucciones. Añadidas restricciones de seguridad y comprobaciones.

--- src/screens/OptimiceRouteScreen.tsx
+++ src/screens/OptimiceRouteScreen.tsx
@@ -24,11 +24,15 @@ const OptimizeRouteScreen: React.FC = () => {
   const [markers, setMarkers] = useState<IMarker[]>([]);
   const [mapName, setMapName] = useState<string>('');
   const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [modal2Visible, setModal2Visible] = useState<boolean>(false);
   const [startMarker, setStartMarker] = useState<IMarker | null>(null);
   const [endMarker, setEndMarker] = useState<IMarker | null>(null);
   const [currentRegion, setCurrentRegion] = useState<any>(undefined);
   const mapRef = useRef<MapView | null>(null);
   const [optimizeModalVisible, setOptimizeModalVisible] = useState<boolean>(false);
+    // Utiliza el título del marcador para el valor del Picker
+  const [selectedStartMarkerTitle, setSelectedStartMarkerTitle] = useState('');
+  const [selectedEndMarkerTitle, setSelectedEndMarkerTitle] = useState('');
 
 
   useEffect(() => {
@@ -80,6 +84,7 @@ const OptimizeRouteScreen: React.FC = () => {
               };
             });
             setMarkers(newMarkers);
+            //console.log(newMarkers); // Esto te mostrará la lista de marcadores en la consola
             if (newMarkers.length > 0 && mapRef.current) {
               const coordinates = newMarkers.map(marker => marker.coordinates);
               mapRef.current.fitToCoordinates(coordinates, {
@@ -102,12 +107,19 @@ const OptimizeRouteScreen: React.FC = () => {
   };
 
   const handleGetDirections = () => {
+
+    //Para usarlo con MAPS maximo 9 ubicaciones
+    if (markers.length > 9) {
+      Alert.alert('Demasiados marcadores', 'Google Maps no puede optimizar rutas con más de 9 puntos. Usa nuestro software para poder optimizar hasta 23 puntos.');
+      return;
+    }
+
     if (!startMarker || !endMarker) {
       Alert.alert('Error', 'Por favor, selecciona un marcador de inicio y un marcador de fin.');
       return;
     }
 
-    console.log('Optimizando ruta entre:', startMarker.title, 'y', endMarker.title);
+    //console.log('Optimizando ruta entre:', startMarker.title, 'y', endMarker.title);
     // Cerrar el modal después de iniciar la optimización
     setOptimizeModalVisible(false);
     // Optimizar la ruta utilizando los marcadores seleccionados
@@ -126,7 +138,27 @@ const OptimizeRouteScreen: React.FC = () => {
 
     getDirections(data);
   };
-  
+
+  const handleOptimizeButtonPress = () => {
+    if (markers.length === 0) {
+      Alert.alert('Carga una ruta', 'Para poder optimizar una ruta primero debes cargarla.');
+    } else {
+      setOptimizeModalVisible(true);
+    }
+  };
+
+  // Cuando se selecciona un valor en el Picker, encuentra el marcador correspondiente y actualiza el estado
+  const handleStartMarkerChange = (itemValue: string) => {
+    const marker = markers.find(m => m.title === itemValue);
+    setStartMarker(marker || null);
+    setSelectedStartMarkerTitle(itemValue);
+  };
+
+  const handleEndMarkerChange = (itemValue: string) => {
+    const marker = markers.find(m => m.title === itemValue);
+    setEndMarker(marker || null);
+    setSelectedEndMarkerTitle(itemValue);
+  };
 
   return (
     <View style={styles.container}>
@@ -164,7 +196,7 @@ const OptimizeRouteScreen: React.FC = () => {
           animationType="slide"
           transparent={true}
           visible={modalVisible}
-          onRequestClose={() => setModalVisible(false)}
+          onRequestClose={() => setModal2Visible(false)}
         >
           <View style={styles.centeredModalView}>
             <View style={styles.modalView}>
@@ -182,59 +214,107 @@ const OptimizeRouteScreen: React.FC = () => {
           </View>
         </Modal>
   
-        {/* Modal para optimizar la ruta */}
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={optimizeModalVisible}
-          onRequestClose={() => setOptimizeModalVisible(false)}
-        >
-          <View style={styles.centeredModalView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>Selecciona punto de inicio:</Text>
+    {/* Modal para optimizar la ruta */}
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={optimizeModalVisible}
+      onRequestClose={() => setOptimizeModalVisible(false)}
+    >
+      <View style={styles.centeredModalView}>
+        <View style={styles.optimizeModalView}>
+          <Text style={styles.modalText}>Selecciona el origen:</Text>
+          
+          <Picker
+            selectedValue={startMarker ? startMarker.title : ''}
+            onValueChange={(itemValue, itemIndex) =>
+              setStartMarker(markers.find(marker => marker.title === itemValue) || null)
+            }
+            style={styles.picker}
+          >
+            {markers.map((marker, index) => (
+              <Picker.Item key={index} label={marker.title} value={marker.title} />
+            ))}
+          </Picker>
 
-              <Picker
-                selectedValue={startMarker ? startMarker.title : ''}
-                onValueChange={(itemValue, itemIndex) =>
-                  setStartMarker(markers.find(marker => marker.title === itemValue) || null)
-                }
-                style={styles.picker}
-              >
-                {markers.map((marker, index) => (
-                  <Picker.Item key={index} label={marker.title} value={marker.title} />
-                ))}
-              </Picker>
+          <Text style={styles.modalText}>Selecciona el destino:</Text>
+
+          <Picker
+            selectedValue={endMarker ? endMarker.title : ''}
+            onValueChange={(itemValue, itemIndex) =>
+              setEndMarker(markers.find(marker => marker.title === itemValue) || null)
+            }
+            style={styles.picker}
+          >
+            {markers.map((marker, index) => (
+              <Picker.Item key={index} label={marker.title} value={marker.title} />
+            ))}
+          </Picker>
+
+          {/* Botón de cierre con una X en la esquina */}
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => setOptimizeModalVisible(false)}
+          >
+            <Text style={styles.closeButtonText}>X</Text>
+          </TouchableOpacity>
+
+          <Button
+            title="Optimizar con MAPS"
+            onPress={handleGetDirections}
+            color={markers.length > 9 ? "gray" : "#2196F3"}
+          />
+          <Button title="Optimizar Ruta" onPress={() => console.log("Optimizador normal")} />
+
+            {/* Modal de información */}
+
+            <TouchableOpacity onPress={() => setModal2Visible(true)} style={styles.aboutUsButton}>
+              <Text style={styles.aboutUsText}>Info</Text>
+            </TouchableOpacity>
+
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modal2Visible}
+              onRequestClose={() => setModal2Visible(false)}
+            >
+              <View style={styles.centeredModalView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalTitleText}>
+                  En esta sección puedes elegir optimizar tu ruta de dos formas diferentes:
+                  </Text>
+                  {/* Lista de opciones */}
+                  <View style={styles.optionList}>
+                    <Text style={styles.optionItem}>
+                      <Text style={styles.optionTitle}>• Con google MAPS: </Text>
+                      Con google MAPS (permite hasta 9 puntos), y te guia por ellos
+                    </Text>
+                    <Text style={styles.optionItem}>
+                      <Text style={styles.optionTitle}>• Con nuestro software: </Text>
+                      Con nuestro software que permite hasta 23 puntos pero no te guía por ellos, te dibuja la líne que deberás seguir y tu puedes caminar entre los puntos, o pasarlos a Google Maps y que te guíe.
+                    </Text>
+                  </View>
+
+                  <TouchableOpacity
+                    style={styles.closeModalButton}
+                    onPress={() => setModal2Visible(false)}
+                  >
+                    <Text style={styles.closeModalButtonText}>Cerrar</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
+
+        </View>
+      </View>
+    </Modal>
   
-              <Text style={styles.modalText}>Selecciona punto final:</Text>
+        <Button
+          title="Optimizar Ruta"
+          onPress={handleOptimizeButtonPress}
+          color={markers.length === 0 ? "gray" : "#2196F3"}
+        />
 
-              <Picker
-                selectedValue={endMarker ? endMarker.title : ''}
-                onValueChange={(itemValue, itemIndex) =>
-                  setEndMarker(markers.find(marker => marker.title === itemValue) || null)
-                }
-                style={styles.picker}
-              >
-                {markers.map((marker, index) => (
-                  <Picker.Item key={index} label={marker.title} value={marker.title} />
-                ))}
-              </Picker>
-
-
-  
-              {/* Botón de cierre con una X en la esquina */}
-              <TouchableOpacity
-                style={styles.closeButton}
-                onPress={() => setOptimizeModalVisible(false)}
-              >
-                <Text style={styles.closeButtonText}>X</Text>
-              </TouchableOpacity>
-
-              <Button title="Optimizar Ruta" onPress={handleGetDirections} />
-            </View>
-          </View>
-        </Modal>
-  
-        <Button title="Optimizar Ruta" onPress={() => setOptimizeModalVisible(true)} />
       </View>
   
     </View>
@@ -295,6 +375,24 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
+  optimizeModalView: {
+    // Estilos específicos para el modal de optimización de ruta
+    width: '70%', // Anchura como porcentaje del ancho de la pantalla
+    height: '55%', // Altura como porcentaje de la altura de la pantalla
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    justifyContent: 'space-around', // Cambia a 'space-around' para una mejor distribución
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
   closeModalButton: {
     backgroundColor: '#2196F3',
     borderRadius: 20,
@@ -307,7 +405,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   modalText: {
-    marginBottom: 15,
+    marginBottom: 15,  // Aumenta el margen inferior para separar del selector
     textAlign: 'center',
   },
   closeButton: {
@@ -326,10 +424,34 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   picker: {
-  width: '100%', // o el ancho que necesites
-  height: 70, // o la altura que necesites
-  // ...otros estilos que necesites para el Picker
-},
+    width: '100%', // El ancho se ajusta al ancho del modal
+    height: 50, // Altura del Picker
+    borderColor: 'grey', // Color del borde del Picker
+    borderWidth: 1, // Ancho del borde del Picker
+    marginBottom: 20, // Agrega margen inferior para separarlo del próximo texto o elemento
+  },
+  optimizeButton: {
+    marginTop: 20, // Aumenta el margen superior para separar del último Picker
+    width: '80%', // Ajusta el ancho del botón si es necesario
+  },
+  // ... (resto de tus estilos)
+  modalTitleText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  optionList: {
+    alignItems: 'flex-start',
+    marginBottom: 15,
+  },
+  optionItem: {
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  optionTitle: {
+    fontWeight: 'bold',
+  },
 });
 
 
