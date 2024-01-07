767445515c4bd6a764ef725f063bb461aed96b7f
Abraham Cordoba Perez <82827135+abrahamcp02@users.noreply.github.com>
2024-01-07 18:28:17 +0100

    Update RutasPredefinidas.tsx

--- src/data/RutasPredefinidas.tsx
+++ src/data/RutasPredefinidas.tsx
@@ -2,15 +2,33 @@ export const MalagaKML = `<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 <Document>
   <Placemark>
-    <name>Ubi 1</name>
+    <name>Plaza de la Marina</name>
     <Point>
-      <coordinates>-4.796186350286007,37.889847810663866</coordinates>
+      <coordinates>-4.42084,36.717854</coordinates>
     </Point>
   </Placemark>
   <Placemark>
-    <name>Ubi 2</name>
+    <name>Calle Larios</name>
     <Point>
-      <coordinates>-4.7821298986673355,37.897583707217926</coordinates>
+      <coordinates>-4.42143,36.718215</coordinates>
+    </Point>
+  </Placemark>
+  <Placemark>
+    <name>Plaza de la Constitución</name>
+    <Point>
+      <coordinates>-4.421509,36.718699</coordinates>
+    </Point>
+  </Placemark>
+  <Placemark>
+    <name>Plaza de la Merced</name>
+    <Point>
+      <coordinates>-4.418254,36.721982</coordinates>
+    </Point>
+  </Placemark>
+  <Placemark>
+    <name>Calle Molina Larios</name>
+    <Point>
+      <coordinates>-4.420132,36.720778</coordinates>
     </Point>
   </Placemark></Document></kml>`;
 
