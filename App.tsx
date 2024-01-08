315d4e0fbe56f6748d2abf75a8f3301a249d61a1
Alfonso Cabezas Fernández <alfonso.acf66@gmail.com>
2024-01-08 03:36:16 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,72 +1,66 @@
-// App.js
 import React, { useEffect } from 'react';
 import { Alert, SafeAreaView, StatusBar, useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigator from './src/navigation/Navigator';
 import NfcManager, { NfcEvents, Ndef } from 'react-native-nfc-manager';
-import { NfcData } from './src/types/nfcTypes'; // Asegúrate de que la ruta de importación sea correcta
-
 
 // Importa los datos NFC, asegurándote de que la ruta sea correcta
 import rawData from './src/data/nfcData.json';
-const nfcData: NfcData = rawData as NfcData;
+const nfcData: { [key: string]: string } = rawData as { [key: string]: string };
+
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
 
   useEffect(() => {
     async function initNfc() {
       const supported = await NfcManager.isSupported();
       if (supported) {
-        const enabled = await NfcManager.isEnabled();
-        if (!enabled) {
-          // NFC está soportado pero no activado
-          Alert.alert(
-            "NFC is not enabled",
-            "Please enable NFC to use this application.",
-            [
-              // Opcionalmente, puedes dirigir al usuario a la configuración de NFC
-              { text: "Go to Settings", onPress: () => NfcManager.goToNfcSetting() },
-              { text: "OK" }
-            ]
-          );
-        } else {
-          // NFC está activado y listo para usarse
-          NfcManager.start();
-        }
+        await NfcManager.start();
+        // Escuchar continuamente los tags NFC
+        NfcManager.setEventListener(NfcEvents.DiscoverTag, handleNfcRead);
+        NfcManager.registerTagEvent();
       } else {
-        // NFC no está soportado
         Alert.alert("NFC not supported", "This device doesn't support NFC.");
       }
     }
 
     initNfc();
 
-    NfcManager.setEventListener(NfcEvents.DiscoverTag, (event: any) => {
-      const handleNfcRead = (event: any) => {
-        const ndefRecords = event.ndefMessage;
-        if (ndefRecords) {
-          const id = Ndef.text.decodePayload(ndefRecords[0].payload);
-          const description = nfcData[id];
-          if (description) {
-            Alert.alert('NFC Tag ID', `ID: ${id}`, [
-              { text: 'OK' },
-              { text: 'Show Description', onPress: () => Alert.alert('Description', description) },
-            ]);
-          } else {
-            Alert.alert('NFC Tag ID', 'No description found for this tag.');
-          }
-        }
-      };
-    });
-
-    // Limpiar el listener al desmontar
     return () => {
       NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+      NfcManager.unregisterTagEvent();
     };
   }, []);
 
+  const handleNfcRead = (tag:any) => {
+    try {
+      const ndefRecords = tag.ndefMessage;
+
+      if (ndefRecords && ndefRecords.length > 0) {
+        let payloadBytes = ndefRecords[0].payload;
+
+        if (payloadBytes) {
+          let payloadText = Ndef.text.decodePayload(payloadBytes);
+          // Mostrar el texto del payload y buscar en nfcData
+          lookupInNfcData(payloadText);
+        }
+      }
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      // No es necesario cancelar la solicitud de tecnología aquí, ya que queremos seguir escuchando
+    }
+  };
+
+  const lookupInNfcData = (key:any) => {
+    const description = nfcData[key];
+    if (description) {
+      Alert.alert('Info del Monumento', description, [{ text: 'Ok', onPress: () => {} }]);
+    } else {
+      Alert.alert('Tag NFC Irreconocible', "Descripción no encontrada", [{ text: 'Ok', onPress: () => {} }]);
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: isDarkMode ? '#333' : '#FFF' }}>
